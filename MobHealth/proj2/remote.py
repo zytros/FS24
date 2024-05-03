315689babfd28fe42c389da3ebac78e7442945c4
Luca Sichi <luca.sichi@hotmail.com>
2024-05-03 14:28:47 +0200

    add

--- MobHealth/proj2/remote.py
+++ MobHealth/proj2/remote.py
@@ -7,6 +7,8 @@ all_train_data_fn = []
 for i in range(0, 280):
     all_train_data_fn.append(f'data/test/test_trace_{i:03d}.pkl')
 
+print('Number of files:', len(all_train_data_fn))
+
 data_list = []
 
 for i, fn in enumerate(all_train_data_fn):
@@ -18,8 +20,12 @@ for i, fn in enumerate(all_train_data_fn):
         for ts, val in zip(timestamps_altitude, rec.data['altitude'].values)
     ])
 
+print('loaded data')
+
 df = pd.DataFrame(data_list)
 
 extracted_features = extract_features(df, column_id='id', column_sort='time', impute_function=impute)
 
 extracted_features.to_csv('extracted_features_test.csv')
+
+print('saved extraced features to extracted_features_test.csv')
