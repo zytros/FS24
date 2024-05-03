bdf13d1887a5dd12ad2b54453bb1316135192700
Luca Sichi <luca.sichi@hotmail.com>
2024-05-03 14:24:58 +0200

    asdasdasd

---
+++ MobHealth/proj2/remote.py
@@ -0,0 +1,25 @@
+from mhealth_activity import Recording
+import pandas as pd
+from tsfresh import extract_features
+from tsfresh.utilities.dataframe_functions import impute
+
+all_train_data_fn = []
+for i in range(0, 280):
+    all_train_data_fn.append(f'data/test/test_trace_{i:03d}.pkl')
+
+data_list = []
+
+for i, fn in enumerate(all_train_data_fn):
+    rec = Recording(fn)
+    timestamps_altitude = rec.data['altitude'].timestamps
+    
+    data_list.extend([
+        {"id": i, "time": ts, "value": val}
+        for ts, val in zip(timestamps_altitude, rec.data['altitude'].values)
+    ])
+
+df = pd.DataFrame(data_list)
+
+extracted_features = extract_features(df, column_id='id', column_sort='time', impute_function=impute)
+
+extracted_features.to_csv('extracted_features_test.csv')
